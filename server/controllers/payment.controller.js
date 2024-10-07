345adc0dd3af35b3f20bc1616d61dca010dae579
Egman123 <59998379+Egman123@users.noreply.github.com>
2024-10-07 14:28:25 +0400

    Update payment.controller.js

--- server/controllers/payment.controller.js
+++ server/controllers/payment.controller.js
@@ -41,8 +41,8 @@ export const createCheckoutSession = async (req, res) => {
 			payment_method_types: ["card"],
 			line_items: lineItems,
 			mode: "payment",
-			success_url: `${process.env.CLIENT_URL}/purchase-success?session_id={CHECKOUT_SESSION_ID}`,
-			cancel_url: `${process.env.CLIENT_URL}/purchase-cancel`,
+			success_url: `https://nc-mern-shop.onrender.com/purchase-success?session_id={CHECKOUT_SESSION_ID}`,
+			cancel_url: `https://nc-mern-shop.onrender.com/purchase-cancel`,
 			discounts: coupon
 				? [
 						{
